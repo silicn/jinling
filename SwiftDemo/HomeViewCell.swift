ef12a030d0bd806a91d7bcae7776e287ae2666fe
silicn <guanghui.shi@quantgroup.cn>
2019-05-13 17:44:39 +0800

    add handyJSON

---
+++ SwiftDemo/HomeViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  HomeViewCell.swift
+//  SwiftDemo
+//
+//  Created by silicn on 2019/5/13.
+//  Copyright © 2019 Shiguanghui. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
--- SwiftDemo/HomeViewController.swift
+++ SwiftDemo/HomeViewController.swift
@@ -14,13 +14,22 @@ class HomeViewController: UIViewController {
 
     
     var tableView:UITableView?
+    var dataSource:Array<KnowEntity>?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = UIColor.cyan
         
+        self.tableView = UITableView(frame: CGRect(x: 0,y: 0,width: UIScreen.main.bounds.size.width,height: UIScreen.main.bounds.size.height), style: UITableView.Style.plain);
         
+        self.tableView?.delegate = self as UITableViewDelegate
+        self.tableView?.dataSource = self as UITableViewDataSource
         
+        self.view.addSubview(self.tableView!);
+        
+        self.tableView?.register(UINib.init(nibName: "HomeViewCell", bundle: nil), forCellReuseIdentifier: "HomeViewCell")
+        
+        requestData()
         
         
 
@@ -39,15 +48,38 @@ class HomeViewController: UIViewController {
     */
 }
 
-extension HomeViewController {
+
+extension HomeViewController:UITableViewDataSource,UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        return 20;
+    }
+    
+    
+    // Row display. Implementers should *always* try to reuse cells by setting each cell's reuseIdentifier and querying for available reusable cells with dequeueReusableCellWithIdentifier:
+    // Cell gets various attributes set automatically based on table (separators) and data source (accessory views, editing controls)
+    
+    @available(iOS 2.0, *)
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
+         let cell = tableView.dequeueReusableCell(withIdentifier: "HomeViewCell") as! HomeViewCell
+        return cell
+    }
+    
     func requestData(){
-        let urlString = ""
+        let urlString = "https://www.ratjin.com/rat/topic/list"
         let url:URL = URL(string: urlString)!
         
         Alamofire.request(url,method:.post).responseJSON { (response) in
             if response.result.isSuccess {
                 print(response.result.value as Any)
-                var dataSource:Array<KnowEntity>?
+                
+                if let json = response.result.value {
+                    let dic = try? JSONSerialization.jsonObject(with: response.data!, options: JSONSerialization.ReadingOptions.allowFragments)
+                    let array = dic?["list"]
+                    for dictionary in array{
+                    }
+                }
+
                 
             }else{
                 print("request error")
